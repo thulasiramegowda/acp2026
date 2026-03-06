3cb06fe9da0a68e128e53c5f3992231d803308f8
thulasiramegowda <thulasiramegowda29@gmail.com>
2026-03-06 07:25:56 +0530

    Completed act7 correct program

--- level1/act7.c
+++ level1/act7.c
@@ -1,39 +1,51 @@
 #include <stdio.h>
 #include <string.h>
 
-void input(char *str);
-void concatenate_strings(char *str1, char *str2);
-void display(char *str);
+void inputStrings(char str1[], char str2[]);
+int compareStrings(char str1[], char str2[]);
+void output(int result);
 
 int main()
 {
     char str1[100], str2[100];
+    int result;
 
-    printf("Enter first string: ");
-    input(str1);
+    inputStrings(str1, str2);
 
-    printf("Enter second string: ");
-    input(str2);
+    result = compareStrings(str1, str2);
 
-    concatenate_strings(str1, str2);
-
-    printf("Concatenated string: ");
-    display(str1);
+    output(result);
 
     return 0;
 }
 
-void input(char *str)
+void inputStrings(char str1[], char str2[])
 {
-    scanf("%s", str);
+    printf("Enter first string: ");
+    scanf("%s", str1);
+
+    printf("Enter second string: ");
+    scanf("%s", str2);
 }
 
-void concatenate_strings(char *str1, char *str2)
+int compareStrings(char str1[], char str2[])
 {
-    strcat(str1, str2);
+    int res = strcmp(str1, str2);
+
+    if(res == 0)
+        return 0;
+    else if(res > 0)
+        return 1;
+    else
+        return -1;
 }
 
-void display(char *str)
+void output(int result)
 {
-    printf("%s\n", str);
+    if(result == 0)
+        printf("Both strings are equal\n");
+    else if(result == 1)
+        printf("First string is lexicographically greater\n");
+    else
+        printf("Second string is lexicographically greater\n");
 }
