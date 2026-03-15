e5530c255cd9c37ee0eeee44197add4c961c6316
thulasiramegowda <thulasiramegowda29@gmail.com>
2026-03-15 21:23:27 +0530

    Re-submitting activity1 code

--- level1/act1.c
+++ level1/act1.c
@@ -1,44 +1,43 @@
-#include<stdio.h>
-void input(int n, float arr[n]);
-int find_max_index(int n, float arr[n]);
-void output(float arr[], int max_index);
+#include <stdio.h>
 
-int main() {
-    int n;
-    printf("Enter number of elements: ");
-    scanf("%d", &n);
+void inputTemperatures(int n, float temps[n]);
+float findHighest(int n, float temps[n]);
+float findLowest(int n, float temps[n]);
+void output(float max, float min);
 
-    float arr[n];
-
-    input(n, arr);
-
-    int max_index = find_max_index(n, arr);
-
-    output(arr, max_index);
-
+int main(){
+    int n=7;
+    float max,min;
+    float temps[7];
+    inputTemperatures(n,temps);
+    max = findHighest(n,temps);
+    min = findLowest(n,temps);
+    output(max,min);
     return 0;
 }
-
-void input(int n, float arr[n]) {
-    printf("Enter %d float numbers:\n", n);
-    for(int i = 0; i < n; i++) {
-        scanf("%f", &arr[i]);
+void inputTemperatures(int n, float temps[n]){
+    for(int i=0;i<n;i++){
+        printf("enter the temp of %d day :",i+1);
+        scanf("%f",&temps[i]);
     }
 }
-
-int find_max_index(int n, float arr[n]) {
-    int max_index = 0;
-
-    for(int i = 1; i < n; i++) {
-        if(arr[i] > arr[max_index]) {
-            max_index = i;
-        }
+float findHighest(int n, float temps[n]){
+    float max= temps[0];
+    for(int i=0;i<n;i++){
+        if(temps[i]>max)
+        max = temps[i];
     }
-
-    return max_index;
+    return max;
 }
-
-void output(float arr[], int max_index) {
-    printf("Maximum value = %.2f\n", arr[max_index]);
-    printf("Index of maximum value = %d\n", max_index);
+float findLowestt(int n, float temps[n]){
+    float min=temps[0];
+    for(int i=0;i<n;i++){
+        if(temps[i]<min)
+        min = temps[i];
+    }
+    return min;
+}
+void output(float max,float min){
+    printf("the highest temperature is %d",max);
+    printf("the lowest temperature is %d",min);
 }
