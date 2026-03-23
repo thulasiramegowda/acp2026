ad8ebb9c46f2cf9eaf6d0217528e8a6a71a5be04
thulasiramegowda <thulasiramegowda29@gmail.com>
2026-03-23 21:47:09 +0530

    Re-submmiting actvitiy code

--- level1/act3.c
+++ level1/act3.c
@@ -1,75 +1,56 @@
-#include <stdio.h>
-
 typedef struct {
-    float length;
-    float width;
-    float area;
-} Rectangle;
+    	float length;
+    	float width;
+    	float area;
+    	float costPerUnit;
+    	float totalCost;
+} Floor;
 
-void input(int n, Rectangle rects[]);
-void calculate_area(int n, Rectangle rects[]);
-int findLargestArea(int n, Rectangle rects[]);
-void output(int largestIndex, Rectangle rects[]);
+void inputDetails(int n, Floor floors[]);
+void calculateCosts(int n, Floor floors[]);
+int highestCostFloor(int n, Floor floors[]);
+void displayHighestCost(int index, Floor floors[]);
 
-int main()
-{
-    int n;
-    printf("Enter number of rectangles: ");
+int main(){
+    int n, index;
+    printf("Enter number of floors:");
     scanf("%d", &n);
-
-    Rectangle rects[n];
-
-    input(n, rects);
-    calculate_area(n, rects);
-
-    int largestIndex = findLargestArea(n, rects);
-
-    output(largestIndex, rects);
-
-    return 0;
+    Floor floors[n];
+    
+    inputDetails(n,floors);
+    calculateCosts(n, floors);
+    index = highestCostFloor(n, floors);
+    displayHighestCost(index,floors);
+    retrun 0;
+}
+void inputDetails(int n, Floor floors[n]){
+    for(int i=0;i<n;i++){
+    printf("--floar deatils--\n");
+    printf("enter the length :");
+    scanf("%f\n",&floors[i].length);
+printf("enter the width :");
+    scanf("%f\n",&floors[i].width);
+printf("enter the Cost per unit area :");
+    scanf("%f\n",&floors[i].costPerUnit);
+}
 }
 
-void input(int n, Rectangle rects[])
-{
-    for(int i = 0; i < n; i++)
-    {
-        printf("\nRectangle %d\n", i+1);
-
-        printf("Enter length: ");
-        scanf("%f", &rects[i].length);
-
-        printf("Enter width: ");
-        scanf("%f", &rects[i].width);
-    }
+void calculateCosts(int n, Floor floors[]){
+for(int i=0;i<n;i++){
+    floors[i].area = floors[i].length * floors[i].width;
+    floors[i].totalCost = floors[i].area * floors[i].costPerUnit;
 }
-
-void calculate_area(int n, Rectangle rects[])
-{
-    for(int i = 0; i < n; i++)
-    {
-        rects[i].area = rects[i].length * rects[i].width;
-    }
 }
-
-int findLargestArea(int n, Rectangle rects[])
-{
-    int largest = 0;
-
-    for(int i = 1; i < n; i++)
-    {
-        if(rects[i].area > rects[largest].area)
-        {
-            largest = i;
+int highestCostFloor(int n,Floor floors[]){
+    int index=0;
+    for(int i=0;i,n;i++){
+        if(floors[i].totalCost > floors[index].totalCost){
+            index = i;
         }
     }
-
-    return largest;
+    return index;
 }
-
-void output(int largestIndex, Rectangle rects[])
-{
-    printf("\nLargest Rectangle Details:\n");
-    printf("Length = %.2f\n", rects[largestIndex].length);
-    printf("Width = %.2f\n", rects[largestIndex].width);
-    printf("Area = %.2f\n", rects[largestIndex].area);
+void output(int index,Floor floor[]){
+    printf("the floor with highest cost : %d",index + 1);
+    printf("the total cost = %.3f",Floor[index].totalCost);
 }
